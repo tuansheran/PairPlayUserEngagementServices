7c36ce9c5bec838dd7ffbfcd8c7276d78bba18de
sheranbuckmangit <sherantuanbuckman@gmail.com>
2025-07-15 23:33:53 +0530

    feat: update app.set use proxy

--- dist/index.js
+++ dist/index.js
@@ -12,10 +12,10 @@ const apiKeyAuth_1 = require("./middleware/apiKeyAuth");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8080;
+app.set('trust proxy', true);
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.set('trust proxy', true);
 app.use('/', apiKeyAuth_1.apiKeyAuth, userFeed_1.default);
 app.use('/', apiKeyAuth_1.apiKeyAuth, uploadProfilePhoto_1.default);
 app.listen(PORT, () => {
--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,11 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.set('trust proxy', true);
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.set('trust proxy', true)
+
 
 app.use('/', apiKeyAuth, userFeed)
 app.use('/', apiKeyAuth, uploadProfilePhoto)
