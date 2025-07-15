1ab0de9f837481346d490043e377c3b701f60f82
sheranbuckmangit <sherantuanbuckman@gmail.com>
2025-07-15 22:50:00 +0530

    feat: change port 8080

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { apiKeyAuth } from './middleware/apiKeyAuth'
 dotenv.config()
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors())
 app.use(express.json())
