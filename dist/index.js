fe15cc0f216bf7a9be17a67b493b1fa5c8351207
sheranbuckmangit <sherantuanbuckman@gmail.com>
2025-07-15 23:41:18 +0530

    feat: remove api key

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const userFeed_1 = __importDefault(require("./route/userFeed"));
 const uploadProfilePhoto_1 = __importDefault(require("./route/uploadProfilePhoto"));
-const apiKeyAuth_1 = require("./middleware/apiKeyAuth");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8080;
@@ -16,8 +15,8 @@ app.set('trust proxy', true);
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use('/', apiKeyAuth_1.apiKeyAuth, userFeed_1.default);
-app.use('/', apiKeyAuth_1.apiKeyAuth, uploadProfilePhoto_1.default);
+app.use('/', userFeed_1.default);
+app.use('/', uploadProfilePhoto_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
